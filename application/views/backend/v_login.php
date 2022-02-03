a6faa39759e28219a801b8d9a530cc1725d2a406
Rosihan Ardiansyah <rosihan17@gmail.com>
2022-02-03 14:32:10 +0700

    login

--- application/views/backend/v_login.php
+++ application/views/backend/v_login.php
@@ -34,6 +34,7 @@
     </head>
     <body class="page-login">
         <main class="page-content">
+                <img src="bg.png" style="position:fixed;right:0;bottom:0;min-width:100%;min-height:100%;opacity:100%;">
             <div class="page-inner">
                 <div id="main-wrapper">
                     <div class="row">
