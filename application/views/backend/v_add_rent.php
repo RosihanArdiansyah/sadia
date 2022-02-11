4850240c0cb586425944c0654f8bfd25e0d14dde
maulanaarf3105 <maulanabd@gmail.com>
2022-02-11 11:13:55 +0800

    form

--- application/views/backend/v_add_rent.php
+++ application/views/backend/v_add_rent.php
@@ -31,12 +31,7 @@
                                 <div class="panel-body">
                                     <div class="form-group">        
                                         <label>Barang</label>
-                                            <select class="form-control" name="category" required>
-                                                <option value="">-Select Option-</option>
-                                                    <?php foreach ($category->result() as $row) : ?>
-                                                        <option value="<?php echo $row->category_id;?>"><?php echo $row->category_name;?></option>
-                                                    <?php endforeach;?>
-                                            </select>
+                                            <input type="text" name="sum" value="<?php echo $b['category'];?>" class="form-control" class="form-control" required>
                                         <label style="margin-top: 8px;">Jumlah</label>
                                             <input type="number" name="sum" class="form-control" placeholder="Jumlah Barang" required>
                                     </div>
